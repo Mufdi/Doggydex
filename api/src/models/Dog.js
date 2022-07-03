bf678bbd4d8eeb39a054a378e17b8f71a2b847e5
Felipe <mufdi@tuta.io>
2022-07-03 01:20:15 -0400

    Db completed and back in 40%

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,47 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey : true,
+      defaultValue : Sequelize.UUIDV4,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    heightMin: {
+      type: DataTypes.INTEGER,
+      allowNull: false 
+    },
+    heightMax: {
+      type: DataTypes.INTEGER,
+      allowNull: false 
+    },
+    weightMin: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    weightMax: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    life_span: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    created: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dogsRoutes = require('./dogs')
+const temperamentsRoutes = require('./temperaments')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/', dogsRoutes)
+router.use('/:id', dogsRoutes)
+router.use('/create', dogsRoutes)
+router.use('/', temperamentsRoutes)
 
 module.exports = router;
