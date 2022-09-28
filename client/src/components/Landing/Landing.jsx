dad661d5e9bd78ed0a7aaeaedfe132bb7ddda806
Felipe <mufdi@tuta.io>
2022-09-28 20:08:34 -0300

    Vercel setup 15

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -12,8 +12,8 @@ export default function Landing () {
                         <Link to="/dogs">
                             <button className={styles.buttonLand}>
                                 <p>Home</p>
-                                <svg stroke-width="4" stroke="currentColor" viewBox="0 0 24 24" fill="none" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M14 5l7 7m0 0l-7 7m7-7H3" stroke-linejoin="round" stroke-linecap="round"></path>
+                                <svg strokeWidth="4" stroke="currentColor" viewBox="0 0 24 24" fill="none"  xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14 5l7 7m0 0l-7 7m7-7H3" strokeLinejoin="round" strokeLinecap="round"></path>
                                 </svg>
                             </button>
                         </Link>
