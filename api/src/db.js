aa364f4e05849d22c7e79926a1af08555198736b
Felipe <mufdi@tuta.io>
2022-09-27 21:33:45 -0300

    Setup for Vercel3

--- api/src/db.js
+++ api/src/db.js
@@ -9,14 +9,6 @@ const {
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-      ssl: {
-          require: true,
-          rejectUnauthorized: false
-      }
-  }
 });
 const basename = path.basename(__filename);
 
