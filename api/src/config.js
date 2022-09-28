8f3ce68882fae7005dd5717719288fd931fe537d
Felipe <mufdi@tuta.io>
2022-09-28 01:46:17 -0300

    Setup for railwayMIL

--- api/src/config.js
+++ api/src/config.js
@@ -1,9 +1,9 @@
-const PORT = process.env.PORT || 3001
-const DB_USER = process.env.DB_USER || 'mufdidev'
-const DB_PASSWORD = process.env.DB_PASSWORD || '12345'
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_NAME = process.env.DB_NAME || 'dogs'
-const DB_PORT = process.env.DB_PORT || '5432'
+const PORT = process.env.PORT 
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD 
+const DB_HOST = process.env.DB_HOST 
+const DB_NAME = process.env.DB_NAME 
+const DB_PORT = process.env.DB_PORT 
 const API_KEY = '3cb6b302-54e6-465c-9fc7-99e4a6150ef3'
  
 
