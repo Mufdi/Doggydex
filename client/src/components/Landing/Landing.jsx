3fcdc9b32f7ff2cd31856ea568e6a86d822148e4
Felipe <mufdi@tuta.io>
2022-09-28 22:38:19 -0300

    Vercel setup 15

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -5,21 +5,7 @@ import styles from "./Landing.module.css"
 export default function Landing () {
     return (
         <div className={styles.contLanding}>
-            <div className={styles.containerLanding}>
-                <div className={styles.textLanding}>
-                    <h1 className={styles.titleLand}>Welcome to the DoggyDex!!</h1>
-                    <div className={styles.cont}>
-                        <Link to="/dogs">
-                            <button className={styles.buttonLand}>
-                                <p>Home</p>
-                                {/* <svg strokeWidth="4" stroke="currentColor" viewBox="0 0 24 24" fill="none"  xmlns="https://www.w3.org/2000/svg">
-                                <path d="M14 5l7 7m0 0l-7 7m7-7H3" strokeLinejoin="round" strokeLinecap="round"></path>
-                                </svg> */}
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
+            <h1>hola</h1>
         </div>
     )
 }
