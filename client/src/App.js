ddb5b67118f86e108716bbabfa7717ea431b51e6
Felipe <mufdi@tuta.io>
2022-09-29 21:17:40 -0300

    Fixed deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
 import Landing from './components/Landing/Landing'
