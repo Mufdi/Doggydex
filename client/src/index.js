b4a6266031147d4756da7fceaac336e71c9c38bc
Felipe <mufdi@tuta.io>
2022-09-30 00:22:34 -0300

    Fixed deploy

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 import { store } from './redux/store';
+import { unregister } from './registerServiceWorker';
+unregister();
 
 ReactDOM.render(
   <Provider store={store}>
